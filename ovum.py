dba54efaa99c36e48e98198ec20faf90b02595db
Elliot Chance <elliotchance@gmail.com>
2015-02-03 19:54:03 +1100

    test_installing_a_package_that_doesnt_exist_does_not_modify_yml

--- ovum.py
+++ ovum.py
@@ -5,43 +5,59 @@ import os
 __version__ = '1.0'
 __ovum_yml__ = 'ovum.yml'
 
-def fetch_pypi(name):
-    print "Could not find package: %s" % name
+class PyPIPackage:
+    def __init__(self, name):
+        self.name = name
 
-def require(args, dev):
-    key = 'require-dev' if dev else 'require'
+    def fetch(self):
+        if not self.name:
+            return
+        raise RuntimeError("Could not find package: %s" % self.name)
 
-    if len(args) == 0:
-        print "Usage: ovum %s <package>" % key
-        return
+class CLI:
+    def get_package_for_name(self, name):
+        return PyPIPackage(name)
 
-    yml = {}
-    if os.path.exists(__ovum_yml__):
-        with open(__ovum_yml__, 'r') as file:
-            yml = yaml.load(file)
-    else:
-        open(__ovum_yml__, 'w').close()
+    def require(self, args, dev):
+        key = 'require-dev' if dev else 'require'
 
-    if key not in yml:
-        yml[key] = []
+        if len(args) == 0:
+            print "Usage: ovum %s <package>" % key
+            return
 
-    yml[key].append(args[0])
+        try:
+            package = self.get_package_for_name(args[0])
+            package.fetch()
+        except RuntimeError as e:
+            print e
+            return
 
-    with open(__ovum_yml__, 'w') as file:
-        file.write(yaml.dump(yml, default_flow_style=False))
+        yml = {}
+        if os.path.exists(__ovum_yml__):
+            with open(__ovum_yml__, 'r') as file:
+                yml = yaml.load(file)
+        else:
+            open(__ovum_yml__, 'w').close()
 
-    fetch_pypi(args[0])
+        if key not in yml:
+            yml[key] = []
 
-def main(args):
-    if args[0] == 'require':
-        return require(args[1:], False)
-    elif args[0] == 'require-dev':
-        return require(args[1:], True)
-    elif args[0] == 'install':
-        if not os.path.exists('vendor'):
-            os.mkdir('vendor')
+        yml[key].append(args[0])
 
-    print "ovum v%s" % __version__
+        with open(__ovum_yml__, 'w') as file:
+            file.write(yaml.dump(yml, default_flow_style=False))
+
+    def main(self, args):
+        if args[0] == 'require':
+            return self.require(args[1:], False)
+        elif args[0] == 'require-dev':
+            return self.require(args[1:], True)
+        elif args[0] == 'install':
+            if not os.path.exists('vendor'):
+                os.mkdir('vendor')
+
+        print "ovum v%s" % __version__
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    cli = CLI()
+    cli.main(sys.argv[1:])
--- test_ovum.py
+++ test_ovum.py
@@ -1,9 +1,61 @@
 from unittest import TestCase
 import subprocess
-import ovum
 import os.path
+from ovum import *
+from mock import MagicMock
+import ovum
 
-class TestOvum(TestCase):
+class OvumTestCase(TestCase):
+    def setup_yml(self, yml):
+        with open('ovum.yml', 'w') as file:
+            file.write(yml)
+
+class TestCLI(OvumTestCase):
+    def run_cli(self, args):
+        cli = CLI()
+        cli.get_package_for_name = MagicMock(return_value=PyPIPackage(''))
+        cli.main(args)
+        return cli
+
+    def test_require_will_append_yml_file(self):
+        self.setup_yml('require:\n- "pypi:mock"')
+        self.run_cli(['require', 'pypi:mock2'])
+        with open("ovum.yml", "r") as yml:
+            data = yml.read()
+            self.assertEqual(data, 'require:\n- pypi:mock\n- pypi:mock2\n')
+
+    def test_require_will_create_yml_if_not_exists(self):
+        os.remove('ovum.yml')
+        self.run_cli(['require', 'pypi:mock'])
+        self.assertTrue(os.path.exists('ovum.yml'))
+
+    def test_require_will_setup_yml_file(self):
+        os.remove('ovum.yml')
+        self.run_cli(['require', 'pypi:mock'])
+        with open("ovum.yml", "r") as yml:
+            data = yml.read()
+            self.assertEqual(data, 'require:\n- pypi:mock\n')
+
+    def test_requiredev_will_append_yml_file(self):
+        self.setup_yml('require-dev:\n- "pypi:mock"')
+        self.run_cli(['require-dev', 'pypi:mock2'])
+        with open("ovum.yml", "r") as yml:
+            data = yml.read()
+            self.assertEqual(data, 'require-dev:\n- pypi:mock\n- pypi:mock2\n')
+
+    def test_requiredev_will_create_yml_if_not_exists(self):
+        os.remove('ovum.yml')
+        self.run_cli(['require-dev', 'pypi:mock'])
+        self.assertTrue(os.path.exists('ovum.yml'))
+
+    def test_requiredev_will_setup_yml_file(self):
+        os.remove('ovum.yml')
+        self.run_cli(['require-dev', 'pypi:mock'])
+        with open("ovum.yml", "r") as yml:
+            data = yml.read()
+            self.assertEqual(data, 'require-dev:\n- pypi:mock\n')
+
+class TestOvum(OvumTestCase):
     def run_ovum(self, args):
         command = ['python', 'ovum.py']
         p = subprocess.Popen(command + args, stdout=subprocess.PIPE)
@@ -17,10 +69,6 @@ class TestOvum(TestCase):
         self.assertEqual(err, expected_error)
         self.assertEqual(out, expected_output)
 
-    def setup_yml(self, yml):
-        with open('ovum.yml', 'w') as file:
-            file.write(yml)
-
     def test_cli_version(self):
         self.assert_ovum_output(['--version'], "ovum v1.0\n")
 
@@ -30,47 +78,9 @@ class TestOvum(TestCase):
     def test_require_with_missing_argument(self):
         self.assert_ovum_output(['require'], "Usage: ovum require <package>\n")
 
-    def test_require_will_create_yml_if_not_exists(self):
-        os.remove('ovum.yml')
-        self.run_ovum(['require', 'pypi:mock'])
-        self.assertTrue(os.path.exists('ovum.yml'))
-
-    def test_require_will_setup_yml_file(self):
-        os.remove('ovum.yml')
-        self.run_ovum(['require', 'pypi:mock'])
-        with open("ovum.yml", "r") as yml:
-            data = yml.read()
-            self.assertEqual(data, 'require:\n- pypi:mock\n')
-
-    def test_require_will_append_yml_file(self):
-        self.setup_yml('require:\n- "pypi:mock"')
-        self.run_ovum(['require', 'pypi:mock2'])
-        with open("ovum.yml", "r") as yml:
-            data = yml.read()
-            self.assertEqual(data, 'require:\n- pypi:mock\n- pypi:mock2\n')
-
     def test_requiredev_with_missing_argument(self):
         self.assert_ovum_output(['require-dev'],
                                 "Usage: ovum require-dev <package>\n")
-
-    def test_requiredev_will_create_yml_if_not_exists(self):
-        os.remove('ovum.yml')
-        self.run_ovum(['require-dev', 'pypi:mock'])
-        self.assertTrue(os.path.exists('ovum.yml'))
-
-    def test_requiredev_will_setup_yml_file(self):
-        os.remove('ovum.yml')
-        self.run_ovum(['require-dev', 'pypi:mock'])
-        with open("ovum.yml", "r") as yml:
-            data = yml.read()
-            self.assertEqual(data, 'require-dev:\n- pypi:mock\n')
-
-    def test_requiredev_will_append_yml_file(self):
-        self.setup_yml('require-dev:\n- "pypi:mock"')
-        self.run_ovum(['require-dev', 'pypi:mock2'])
-        with open("ovum.yml", "r") as yml:
-            data = yml.read()
-            self.assertEqual(data, 'require-dev:\n- pypi:mock\n- pypi:mock2\n')
 
     def test_install_creates_vendor_folder(self):
         try:
@@ -83,3 +93,10 @@ class TestOvum(TestCase):
     def test_installing_pypi_package_that_does_not_exist(self):
         self.assert_ovum_output(['require', 'pypi:hkdsfhgfd'],
                                 "Could not find package: pypi:hkdsfhgfd\n")
+
+    def test_installing_a_package_that_doesnt_exist_does_not_modify_yml(self):
+        self.setup_yml('require-dev:\n- pypi:mock\n')
+        self.run_ovum(['require', 'pypi:hkdsfhgfd'])
+        with open("ovum.yml", "r") as yml:
+            data = yml.read()
+            self.assertEqual(data, 'require-dev:\n- pypi:mock\n')
